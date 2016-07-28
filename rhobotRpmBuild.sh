918f37101b29fb47c196cda074d6493ad2d88aec
Francisco Rodriguez <franciso.rodriguez@cfpb.gov>
2016-07-28 16:13:14 -0400

    missed RHOBOTVERSION

--- rhobotRpmBuild.sh
+++ rhobotRpmBuild.sh
@@ -4,7 +4,7 @@ RHOBOTVERSION=0.2
 sed -i -e s#"RHOBOTVERSION"#"${RHOBOTVERSION}"#g rhobot.spec
 
 cp -rpf -R playground rhobot-${RHOBOTVERSION}/
-tar -czf rhobot-0.1.tar.gz rhobot-${RHOBOTVERSION}/
+tar -czf rhobot-${RHOBOTVERSION}.tar.gz rhobot-${RHOBOTVERSION}/
 
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 cp rhobot.spec rpmbuild/SPECS/rhobot.spec
