a84a9b46d3ce7fb4d2ecf5e2af2edc2f950a1990
Frank <franciso.rodriguez@cfpb.gov>
2016-07-29 17:22:18 -0400

    add setup script

---
+++ rhobotSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x #echo on
+
+cp /artifacts/rhobotGolangDependencies.tar.gz $WORKSPACE/rhobotGolangDependencies.tar.gz
+wget --quiet -O $WORKSPACE/go1.6.2.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
+
+mkdir $WORKSPACE/rhobot -p
+tar -xzf $WORKSPACE/rhobotGolangDependencies.tar.gz -C $WORKSPACE/rhobot/
+tar -xzf $WORKSPACE/go1.6.2.linux-amd64.tar.gz -C $WORKSPACE/rhobot/
