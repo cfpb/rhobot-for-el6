c532b626252bf3f5a62a806c0e9317ac003eb39c
Frank <franciso.rodriguez@cfpb.gov>
2016-08-01 13:44:42 -0400

    set default for RHOBOTVERSION if not defined as ENV variable

--- rhobotRpmBuild.sh
+++ rhobotRpmBuild.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -x #echo on
 
-RHOBOTVERSION=0.2
-sed -i -e s#"RHOBOTVERSION"#"${RHOBOTVERSION}"#g rhobot.spec
+RHOVERS=${RHOBOTVERSION:-0.2}
 
-rm -rf rhobot-${RHOBOTVERSION}/
-cp -rpf -R ../rhobot rhobot-${RHOBOTVERSION}/
-tar -czf rhobot-${RHOBOTVERSION}.tar.gz rhobot-${RHOBOTVERSION}/
+sed -i -e s#"RHOBOTVERSION"#"${RHOVERS}"#g rhobot.spec
+
+rm -rf rhobot-${RHOVERS}/
+cp -rpf -R ../rhobot rhobot-${RHOVERS}/
+tar -czf rhobot-${RHOVERS}.tar.gz rhobot-${RHOVERS}/
 
 rm -rf rpmbuild
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 cp rhobot.spec rpmbuild/SPECS/rhobot.spec
-mv rhobot-${RHOBOTVERSION}.tar.gz rpmbuild/SOURCES
+mv rhobot-${RHOVERS}.tar.gz rpmbuild/SOURCES
 
 cd rpmbuild/
 rpmbuild --define "_topdir `pwd`" -ba SPECS/rhobot.spec
