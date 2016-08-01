2a90f5e3f16bbefa2f07dacc16278c48816eebfe
Frank <franciso.rodriguez@cfpb.gov>
2016-08-01 10:52:55 -0400

    move one dir up

--- rhobotSetup.sh
+++ rhobotSetup.sh
@@ -5,5 +5,5 @@ cp /artifacts/rhobotGolangDependencies.tar.gz $WORKSPACE/rhobotGolangDependencie
 wget --quiet -O $WORKSPACE/go1.6.2.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 
 mkdir $WORKSPACE/rhobot -p
-tar -xzf $WORKSPACE/rhobotGolangDependencies.tar.gz -C $WORKSPACE/rhobot/go/
+tar -xzf $WORKSPACE/rhobotGolangDependencies.tar.gz -C $WORKSPACE/rhobot
 tar -xzf $WORKSPACE/go1.6.2.linux-amd64.tar.gz -C $WORKSPACE/rhobot/
