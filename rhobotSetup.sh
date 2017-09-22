c8243b1622201cb57e584591d29fa49c56f59ce9
frank0631 <frank0631@gmail.com>
2017-09-22 20:26:40 +0100

    unpack go download into goroot

--- rhobotSetup.sh
+++ rhobotSetup.sh
@@ -4,4 +4,4 @@ set -x #echo on
 wget --quiet -O $WORKSPACE/go1.8.3.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 mkdir $WORKSPACE/rhobot/goroot -p
 tar -xzf $WORKSPACE/go1.8.3.linux-amd64.tar.gz -C $WORKSPACE/rhobot/
-mv $WORKSPACE/rhobot/go/ /$WORKSPACE/rhobot/gopath/
+mv $WORKSPACE/rhobot/go/ /$WORKSPACE/rhobot/goroot/
