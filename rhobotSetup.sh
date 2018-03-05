e381803aecfd644755c9a35d5daab155f0347f73
Francisco Rodriguez <frank0631@gmail.com>
2018-03-05 16:16:01 -0500

    remove commented out lines

--- rhobotSetup.sh
+++ rhobotSetup.sh
@@ -4,6 +4,3 @@ set -x #echo on
 wget --quiet -O $WORKSPACE/go1.8.3.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 mkdir $WORKSPACE/rhobot-for-el6/go -p
 tar -xzf $WORKSPACE/go1.8.3.linux-amd64.tar.gz -C $WORKSPACE/rhobot/
-
-# mkdir $WORKSPACE/rhobot-for-el6/gopath/src/github.com/cfpb/rhobot -p
-# cp -rpf $WORKSPACE/rhobot $WORKSPACE/rhobot-for-el6/gopath/src/github.com/cfpb/rhobot
